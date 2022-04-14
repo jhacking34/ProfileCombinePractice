8ffb43c0616dc8b622f1a200c5e27639584a843c
Johnny Hacking <jhacking@bhico.com>
2022-04-14 06:59:09 -0600

    Initial Commit

---
+++ ProfileCombinePractice/ProfileCombinePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileCombinePracticeApp.swift
+//  ProfileCombinePractice
+//
+//  Created by Johnny Hacking on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct ProfileCombinePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
